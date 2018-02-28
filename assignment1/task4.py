57992e67842e3e906f4873eb7489fdf5ee703020
Erik <osinga@strw.leidenuniv.nl>
2018-02-28 11:19:32 +0100

    task 4 almost done. Vectorize?

--- assignment1/task4.py
+++ assignment1/task4.py
@@ -1,7 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
-
 train_data = np.loadtxt('data/train_in.csv',delimiter=',') # Shape is (1707,256)
 train_labels = np.loadtxt('data/train_out.csv') # Shape is (1707,)
 test_data = np.loadtxt('data/test_in.csv',delimiter=',')# Shape is (1000,256)
@@ -11,34 +10,14 @@ digits = range(0,10)
 train_data = train_data.T # make sure X is in shape (256, number of examples)
 test_data = test_data.T
 
-
-'''
-def single_layer_perceptron(learning_rate=0.05):
-
-		extra_row = np.ones(1707) # we think this is why there is +1
-		X = np.append(train_data,extra_row.reshape(1,1707),axis=0)
-		
-		assert X.shape == (257,1707)
-		
-		b = np.random.rand(10,1)
-		w = np.random.rand(257,10)
-		a = np.dot(w.T,X) + b
-		
-		assert a.shape == (10,1707)
-
-		g = np.sign(a)	
-
-		print (g)
-
-		if g[int(train_labels[0])][0] != 1:
-			d = 1 
-			w[6] += learning_rate * d * x
-
-'''
+train_data = np.append(train_data,[np.ones(train_data.shape[1])],axis=0)
+test_data = np.append(test_data, [np.ones(test_data.shape[1])],axis=0)
 
 def train_single_layer_perceptron(learning_rate = 0.05):
-	w = np.random.rand(256,10)  #  + 1  still ?
-	b = np.random.rand(10,1)
+	"""
+	Train on the training set untill 100 % accurate. Return the weights 
+	"""
+	w = np.random.rand(257,10)  # 256 + 1 for the bias 
 
 	bad_class = 1e6
 	while bad_class > 0:
@@ -47,8 +26,8 @@ def train_single_layer_perceptron(learning_rate = 0.05):
 
 		for i in range(train_data.shape[1]):
 			X = train_data[:,i]
-			X = X.reshape(256,1)
-			a = np.dot(w.T,X) + b
+			X = X.reshape(257,1)
+			a = np.dot(w.T,X) 
 			yhat = np.argmax(a)
 			
 			ylabel = int(train_labels[i]) # the digit that it should be
@@ -63,17 +42,19 @@ def train_single_layer_perceptron(learning_rate = 0.05):
 
 		# print ('Number of wrong classifications: ', bad_class)
 
-	return w, b
+	return w
 
+w = train_single_layer_perceptron()
 
-w, b = train_single_layer_perceptron()
-
-def test_single_layer_perceptron(w,b):
+def test_single_layer_perceptron(w):
+	"""
+	Test given weights w on the test set. Print the accuracy.
+	"""
 	bad_class = 0
 	for i in range(test_data.shape[1]):
 		X = test_data[:,i]
-		X = X.reshape(256,1)
-		a = np.dot(w.T,X) + b
+		X = X.reshape(257,1)
+		a = np.dot(w.T,X) 
 		yhat = np.argmax(a)
 		
 		ylabel = int(test_labels[i]) # the digit that it should be
@@ -85,6 +66,5 @@ def test_single_layer_perceptron(w,b):
 	print ('Number of wrong classifications: ', bad_class)
 	print ('Accuracy: ', 1 - bad_class/test_data.shape[1])
 
-
-
-test_single_layer_perceptron(w,b)
+test_single_layer_perceptron(w)
+# 
