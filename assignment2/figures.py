6e1b2880c672bf0c796bd66584d9c2d0d5f7bce9
Erik Osinga <osinga@reusel.strw.leidenuniv.nl>
2018-04-11 12:15:48 +0200

    figures task1 done

--- assignment2/figures.py
+++ assignment2/figures.py
@@ -1,25 +1,35 @@
 
-
 import numpy as np 
 from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set()
 
-def test_set_MLP_default():
+def plot_test_acc(testacc,Title):
 	"""
 	Plots the test accuracy (or loss) for the default settings of the MNIST_mlp 
 	as is on the KERAS git.
 	"""
 
-	testloss = np.load('./test_loss_MLP_default.npy')
-	testacc = np.load('./test_accuracy_MLP_default.npy')
-
 	print ('Mean accuracy %4f, standard deviation: %4f' %(np.mean(testacc),np.std(testacc))) 
 
-	plt.title('Test accuracy for 50 different random initializations')
+	plt.title('Test accuracy for 50 initializations of the ' + Title)
 	plt.hist(testacc)
 	plt.xlabel('Accuracy')
 	plt.ylabel('Count')
-	plt.show()
+	# plt.show()
+	plt.savefig('./figures/'+Title+'accuracy.png')
+	plt.close()
 
-test_set_MLP_default()
+
+# testacc = np.load('./results/test_accuracy_MLP_default.npy')
+# plot_test_acc(testacc,'MLP')
+
+testacc = np.load('./results/test_accuracy_CNN_default.npy')
+plot_test_acc(testacc,'CNN')
+
+testacc = np.load('./results/test_accuracy_CNN_permute.npy')
+plot_test_acc(testacc,'CNN_permuted')
+
+testacc = np.load('./results/test_accuracy_MLP_default_permute.npy')
+plot_test_acc(testacc,'MLP_permuted')
+
