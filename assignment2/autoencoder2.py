d41ff21fbdc934d63ad249ee7edb621b5cc7bd0a
E. Osinga <s1546449@tritanium.liacs.nl>
2018-04-17 13:21:54 +0200

    arabic is autoencoder3

--- assignment2/autoencoder2.py
+++ assignment2/autoencoder2.py
@@ -2,57 +2,72 @@ import keras
 
 from keras.layers import Input, Dense
 from keras.models import Model
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import UpSampling2D
 
 from keras.datasets import mnist
 import numpy as np
+import matplotlib.pyplot as plt
 
-# Trying to autoencode faces
+import glob
+from imageio import imread
+
+import sklearn.model_selection
+
+# Trying to autoencode characters from the simpsons
+# Starting with Bart simpson
 
 batch_size = 128
-num_classes = 10
+num_classes = 1
 epochs = 20
 
 # the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = []
+for i in glob.glob('/data/s1546449/simpsons_dataset/bart_simpson/*.jpg'):
+    image = imread(i)
+    x_train.append(image)
 
-x_train = x_train.reshape(60000, 784)
-x_test = x_test.reshape(10000, 784)
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+# x is (1342,xpixel,ypixel,3)
+x_train = np.asarray(x_train)
 x_train /= 255
-x_test /= 255
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+x_train, x_test = sklearn.model_selection.train_test_split(x_train)
+
+print ('Size of the training data:',x_train.shape)
+print ('Size of the test data:', x_test.shape)
+
+# y_train = keras.utils.to_categorical(y_train, num_classes)
+# y_test = keras.utils.to_categorical(y_test, num_classes)
 
 # this is the size of our encoded representations
 encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
 
 # this is our input placeholder
-input_img = Input(shape=(784,))
+input_img = Input(shape=(None,None,3))
+# Convolutional layer
+x = Conv2D(16,(3,3),activation='relu',padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
 # "encoded" is the encoded representation of the input
-encoded = Dense(encoding_dim, activation='relu')(input_img)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+# at this point the representation is (4, 4, 8) i.e. 128-dimensional
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
 # "decoded" is the lossy reconstruction of the input
-decoded = Dense(784, activation='sigmoid')(encoded)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
 # this model maps an input to its reconstruction
 autoencoder = Model(input_img, decoded)
-
-# Create seperate encoder and decoder models
-encoder = Model(input_img, encoded)
-# create a placeholder for an encoded (32-dimensional) input
-encoded_input = Input(shape=(encoding_dim,))
-# retrieve the last layer of the autoencoder model
-decoder_layer = autoencoder.layers[-1]
-# create the decoder model
-decoder = Model(encoded_input, decoder_layer(encoded_input))
-
-
 autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
 
 autoencoder.fit(x_train, x_train,
@@ -62,13 +77,12 @@ autoencoder.fit(x_train, x_train,
                 validation_data=(x_test, x_test))
 
 
+"""
 # encode and decode some digits
 # note that we take them from the *test* set
 encoded_imgs = encoder.predict(x_test)
 decoded_imgs = decoder.predict(encoded_imgs)
 
-# use Matplotlib 
-import matplotlib.pyplot as plt
 
 n = 10  # how many digits we will display
 plt.figure(figsize=(20, 4))
@@ -104,3 +118,4 @@ for i in range(n):
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)    
 plt.show()
+"""
